04581b1d354b6e2a2dc991d8c35a3be83a2804a6
gpoat <1208997189@qq.com>
2019-05-31 01:46:08 +0800

    查找排序链表的倒数第K节点

---
+++ src/main/java/com/offer/FindKToTail.java
@@ -0,0 +1,47 @@
+package com.offer;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2019/5/30.
+ */
+public class FindKToTail {
+
+    @Test
+    public void test() {
+    }
+
+    /**
+     * 1 2 3 4 5 6
+     * @param head
+     * @param k
+     * @return
+     */
+    private Node findKToTail(Node head, int k) {
+
+        if(head == null || k == 0) {
+            return null;
+        }
+        Node ahead = head;
+        Node behind = head;
+        for(int i = 0; i < k - 1; i++) {
+            if(ahead.next != null) {
+                ahead = ahead.next;
+            } else {
+                return null;
+            }
+        }
+
+        while(ahead.next != null) {
+            ahead = ahead.next;
+            behind = behind.next;
+        }
+
+        return behind;
+    }
+}
+
+class Node {
+    int val;
+    Node next;
+}
--- src/main/java/com/offer/ReOrder.java
+++ src/main/java/com/offer/ReOrder.java
@@ -2,6 +2,8 @@ package com.offer;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 /**
  * Created by Administrator on 2019/5/30.
  */
@@ -11,6 +13,9 @@ public class ReOrder {
     public void test() {
         System.out.println(4 % 2);
         System.out.println(5 & 1);
+        int[] array = {1,2,3,4,5,6};
+        reOrder(array);
+        System.out.println(Arrays.toString(array));
     }
 
     /**
@@ -26,13 +31,26 @@ public class ReOrder {
         int start = 0;
         int end = array.length - 1;
         while(start < end) {
-            while(start < end ) {
-
+            while(start < end && !isEven(array[start])) {
+                start++;
+            }
+            while(start < end && isEven(array[end])) {
+                end--;
+            }
+            if(start < end) {
+                int temp = array[start];
+                array[start] = array[end];
+                array[end] = temp;
             }
         }
 
     }
 
+    /**
+     *
+     * @param n
+     * @return true:even
+     */
     private boolean isEven(int n) {
         return (n & 1) == 0;
     }
