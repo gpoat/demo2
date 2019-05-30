e822c9d873dae81c47d9f9e5ca6fea08dea9b7c5
gpoat <1208997189@qq.com>
2019-05-31 02:19:01 +0800

    修改工程名称及增加字符串压缩算法

---
+++ src/main/java/com/crack/CompressString.java
@@ -0,0 +1,18 @@
+package com.crack;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2019/5/31.
+ */
+public class CompressString {
+
+    @Test
+    public void test() {
+
+    }
+
+    private StringBuilder compressString(String str) {
+
+    }
+}
