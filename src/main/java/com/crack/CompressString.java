b394682d9c34583d36b5c1670f40542ffe7b4c1b
gpoat <1208997189@qq.com>
2019-05-31 02:45:35 +0800

    字符串压缩

--- src/main/java/com/crack/CompressString.java
+++ src/main/java/com/crack/CompressString.java
@@ -9,10 +9,31 @@ public class CompressString {
 
     @Test
     public void test() {
-
+        System.out.println(compressString("aaabbbbccccccdd"));
     }
 
+    /**
+     * example aaabbbbccccccdd
+     * @param str
+     * @return
+     */
     private StringBuilder compressString(String str) {
+        if(str == null || str.length() == 0) {
+            return null;
+        }
+        StringBuilder result = new StringBuilder();
+        int count = 1;
+        char last = str.charAt(0);
+        for(int i = 1; i < str.length(); i++) {
+            if(last == str.charAt(i)) {
+                count++;
+            } else {
+                result.append(last).append(count);
+                count = 1;
+                last = str.charAt(i);
+            }
+        }
 
+        return result.append(last).append(count);
     }
 }
