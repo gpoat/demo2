8f528c9fdcc2d5f186e937774810e1944f3ab81f
gpoat <1208997189@qq.com>
2019-06-03 03:04:41 +0800

    增加数组工具类及交换方法

---
+++ src/main/java/com/offer/util/ArrayUtil.java
@@ -0,0 +1,13 @@
+package com.offer.util;
+
+/**
+ * Created by Administrator on 2019/6/3.
+ */
+public class ArrayUtil {
+
+    public static void swap(Object[] array, int a, int b) {
+        Object t = array[a];
+        array[a] = array[b];
+        array[b] = t;
+    }
+}
