b981a0281b38dcf0e833c5ae9c8003be8c2d5329
gpoat <1208997189@qq.com>
2019-06-11 05:57:56 +0800

    delete .idea

---
+++ src/main/java/com/offer/GetLeastNumbers.java
@@ -0,0 +1,20 @@
+package com.offer;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2019/6/8.
+ */
+public class GetLeastNumbers {
+
+    @Test
+    public void test() {
+
+    }
+
+    private void getLeastNumbers(int [] nums, int k) {
+        if(nums == null || nums.length == 0) {
+
+        }
+    }
+}
